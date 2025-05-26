163bc4bd79bc7310c5397bf8e56da1eeb82e2e2b
Estéban DESESSARD <e.desessard@burographic.fr>
2025-05-26 17:42:55 +0200

    📝 Documentation des méthodes de la classe `Db.php`

--- Db.php
+++ Db.php
@@ -19,6 +19,16 @@ class Db
     private static array $instance = [];
     private PDO $conn;
 
+    /**
+     * Constructeur privé pour la classe Db
+     * 
+     * @brief Initialise la connexion à la base de données
+     * @details Crée une nouvelle connexion PDO à la base de données spécifiée
+     * en utilisant les configurations définies dans getDbConfig
+     * 
+     * @param string $dbName Le nom de la base de données à laquelle se connecter
+     * @throws PDOException Si la connexion à la base de données échoue
+     */
     private function __construct(string $dbName)
     {
         $config = self::getDbConfig($dbName);
@@ -32,10 +42,26 @@ class Db
         }
     }
 
+    /**
+     * Méthode clone privée pour empêcher la duplication de l'instance
+     * 
+     * @brief Empêche la duplication de l'instance de la classe Db
+     */
     private function __clone()
     {
     }
 
+    /**
+     * Méthode statique pour obtenir la configuration de la base de données
+     * 
+     * @brief Retourne la configuration de la base de données spécifiée
+     * @details Renvoie un tableau contenant les informations de connexion
+     * pour la base de données demandée
+     * 
+     * @param string $dbName Le nom de la base de données
+     * @return array La configuration de la base de données
+     * @throws InvalidArgumentException Si le nom de la base de données n'est pas reconnu
+     */
     private static function getDbConfig(string $dbName): array
     {
         $config = [
@@ -60,6 +86,15 @@ class Db
         return $config[$dbName];
     }
 
+    /**
+     * Méthode statique pour obtenir une instance de la classe Db
+     * 
+     * @brief Retourne une instance de la classe Db pour la base de données spécifiée
+     * @details Si l'instance n'existe pas, elle est créée. Sinon, l'instance existante est retournée.
+     * 
+     * @param string $dbName Le nom de la base de données à laquelle se connecter
+     * @return Db L'instance de la classe Db pour la base de données spécifiée
+     */
     public static function getInstance(string $dbName): Db
     {
         if (!array_key_exists($dbName, self::$instance)) {
@@ -68,11 +103,29 @@ class Db
         return self::$instance[$dbName];
     }
 
+    /**
+     * Méthode pour obtenir la connexion PDO
+     * 
+     * @brief Retourne l'objet PDO de la connexion à la base de données
+     * @details Permet d'accéder à la connexion pour exécuter des requêtes
+     * 
+     * @return PDO L'objet PDO de la connexion à la base de données
+     */
     public function getConnection(): PDO
     {
         return $this->conn;
     }
 
+    /**
+     * Méthode pour exécuter une requête SQL
+     * 
+     * @brief Exécute une requête SQL préparée avec des paramètres
+     * @details Permet d'exécuter des requêtes SQL en utilisant des paramètres pour éviter les injections SQL
+     * 
+     * @param string $sql La requête SQL à exécuter
+     * @param array $params Les paramètres à lier à la requête
+     * @return PDOStatement Le résultat de la requête exécutée
+     */
     public function query(string $sql, array $params = [])
     {
         $stmt = $this->conn->prepare($sql);
