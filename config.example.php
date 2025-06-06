d4797e8f1a087191d41a68b88701e72c58982bc6
Estéban DESESSARD <e.desessard@burographic.fr>
2025-06-06 09:40:05 +0200

    🔧 Ajout d'un fichier `config.example`

---
+++ config.example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file     config.php
+ * @author   Estéban DESESSARD
+ * @brief    Fichier de configuration de l'application
+ * @details  Contient les différentes constantes nécessaires au fonctionnement de l'application
+ * @date     02/06/2025
+ * @version  0.0
+ */
+
+// Configuration de la base de données Batigest
+define('DB_SERVER', ''); // Insérer le nom du serveur SQL Server
+define('DB_NAME', ''); // Insérer le nom de la base de données Batigest
+define('DB_USERNAME', null);
+define('DB_PASSWORD', null);
